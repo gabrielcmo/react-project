8b11ccf2935282b91e764ce167a803cbf1f0c461
Gabriel Carlos Martins de Oliveira <gabriel@gabrielcmo>
2021-11-29 13:45:53 -0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Toilet Paper', 
+      price: 10.00, 
+      date: new Date(2020, 1, 13),
+    },
+    {
+      id: 2,
+      title: 'Car Insurence', 
+      price: 200.00, 
+      date: new Date(2020, 2, 28),
+    },
+    {
+      id: 3,
+      title: 'New Desk (Windows)', 
+      price: 200.00, 
+      date: new Date(2020, 13, 2),
+    },
+    {
+      id: 4,
+      title: 'Luiza', 
+      price: 99999.99, 
+      date: new Date(2021, 13, 22),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello World! Expenses!</h2>
+      <ExpenseItem 
+        title={expenses[0].title}
+        price={expenses[0].price}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        price={expenses[1].price}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        price={expenses[2].price}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        price={expenses[3].price}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
